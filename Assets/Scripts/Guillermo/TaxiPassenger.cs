032398b5e96d96e39a71c99860d3c2cdbb32cdc4
Mtrx283 <g.barcelli.j@gmail.com>
2025-10-29 15:15:44 -0500

    Recuperación de script

---
+++ Assets/Scripts/Guillermo/TaxiPassenger.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TaxiPassenger : MonoBehaviour
+{
+
+    public GameObject passenger;
+    public Transform dropOffPoint;
+    private bool hasPassenger = false;
+    private bool hasBeenDroppedOff = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == passenger && !hasPassenger && !hasBeenDroppedOff)
+        {
+            hasPassenger = true;
+            passenger.SetActive(false);
+        }
+
+        if (other.CompareTag("Drop") && hasPassenger)
+        {
+            hasPassenger = false;
+            hasBeenDroppedOff = true;
+            passenger.transform.position = dropOffPoint.position;
+            passenger.SetActive(true);
+        }
+    }
+
+}
